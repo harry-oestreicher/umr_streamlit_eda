d39c7bff0c9feed44bd18600adea89bb37f867dc
Harry Oestreicher <harryo1968@gmail.com>
2023-03-30 22:37:52 -0700

    update

--- pages/01_🌈_Holoviews.py
+++ pages/01_🌈_Holoviews.py
@@ -1,33 +1,24 @@
-import datetime
 import os
 import sys
 import time
 import math
 import pathlib
 import requests
-# import zipfile
+import datetime
 import pandas as pd
-# import pydeck as pdk
-# import geopandas as gpd
 import streamlit as st
-# import leafmap.colormaps as cm
-# from leafmap.common import hex_to_rgb
-# from pydeck.types import String
-
 import hvplot.pandas
 import panel as pn
 import holoviews as hv
+import bokeh.plotting
+
 from holoviews import opts
 hv.extension('bokeh')
 
 sys.path.append("..")
 from data.dictionary import get_dictionary
 INDICATOR_dict = get_dictionary('INDICATOR')
-
-# # load the world dataset
-# world = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
-# cities = gpd.read_file(gpd.datasets.get_path('naturalearth_cities'))
-
+REF_AREA_dict = get_dictionary('REF_AREA')
 
 # Begin Streamlit
 # Initialize a session state variable that tracks the sidebar state (either 'expanded' or 'collapsed').
@@ -38,7 +29,7 @@ if 'sidebar_state' not in st.session_state:
 st.set_page_config(initial_sidebar_state=st.session_state.sidebar_state, layout="wide")
 
 # Show title and description of the app.
-st.title("Net Migration Rate 2021")
+st.title("Net Migration Rate")
 st.sidebar.write("""
 **Sidebar**
 
@@ -70,22 +61,22 @@ data_links = {
     },
 }
 
-def get_data_columns(df, category="world", frequency="annual"):
-    cols = df.columns.values.tolist()
-    return cols[1:]
+# def get_data_columns(df, category="world", frequency="annual"):
+#     cols = df.columns.values.tolist()
+#     return cols[1:]
+
+# @st.cache_data
+# def get_reference_data(url):
+#     df = pd.read_csv(url)
+#     df.drop(columns=["Unnamed: 0", "AGE"], inplace=True)
+#     df = df[df.TIME_PERIOD==2021]
+#     return df
 
 @st.cache_data
-def get_reference_data(url):
-    df = pd.read_csv(url)
-    df.drop(columns=["Unnamed: 0", "AGE"], inplace=True)
-    df = df[df.TIME_PERIOD==2021]
-    return df
-
-@st.cache_data
-def get_indicator_data(url):
+def get_data(url):
     df = pd.read_csv(url)
     df.drop(columns=["Unnamed: 0"], inplace=True)
-    df = df[df.TIME_PERIOD==2021]
+    # df = df[df.TIME_PERIOD==2021]
     return df
 
 def get_indicator_dict(name):
@@ -113,75 +104,92 @@ def app():
     st.write(
         """
         ### Exploratory Data Analysis
-
-        Test
           
         """
     )
-
-    row1_col1, row1_col2, row1_col3  = st.columns(
-        [1,1,6]
-    )
-
-    years_list = [2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
-
-    with row1_col1:
-        # st.write("**Year: 2021**")
-        selected_year = st.selectbox("**Year**", years_list )
-
-    # with row1_col2:
-        # indicator = st.selectbox("**Risk Factor Group**", ["DM_", "ECON_", "MG_", "MNCH_", "PT_", "WS_"])
 
     # manually setting these for now
     frequency = "annual"
     scale = "countries"
 
-    indicator_df = get_indicator_data(data_links["indicator"]["_ALL_"])
+    df = get_data(data_links["indicator"]["_ALL_"])
+    years_list = [2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
+    this_df = df.copy()
 
-    with row1_col3:
-        selected_col = "OBS_VALUE" #st.selectbox("Attribute", data_cols, 4)
+    row1_col1, row1_col2  = st.columns([4, 4])
+
+    with row1_col1:
+        # selected_col = "OBS_VALUE" #st.selectbox("Attribute", data_cols, 4)
         # st.write(indicator_df.head(3))
-        this_ind_list = indicator_df["INDICATOR"].unique().tolist()
-        this_indicator = st.selectbox("**Indicator:**", this_ind_list)
+        this_ind_list = get_indicators(this_df)
+        # st.write(type(this_ind_list))
+        # this_ind_list = this_ind_list.sort()
+        this_indicator = st.selectbox("**Indicator:**", sorted(this_ind_list))
         this_indicator_text = get_indicator_dict(this_indicator)
         # Display the indicator full text
-        st.write(this_indicator_text)
-        # st.write(indicator_df["OBS_VALUE"].dtype)
+        st.write(f"**{this_indicator_text}**")
+        # st.write(f"Data type: `{this_df.OBS_VALUE.dtype}`")
+
+    with row1_col2:
+        st.write(".")
+
+
+    ############################################################### Trim the fat 
+    net_mg_rate = this_df[this_df.INDICATOR=='DM_NET_MG_RATE'].sort_values('OBS_VALUE').copy()
+
+    def trim_the_fat(df, num):
+        upp = df.sort_values('OBS_VALUE').head(num)
+        lwr = df.sort_values('OBS_VALUE').tail(num)
+        out = pd.concat([upp,lwr])
+        return out.sort_values('REF_AREA', ascending=False)
+
+    # Trim extremes (top-5 and bottom-5) into a new dataframe.
+    top_40_nmr = trim_the_fat(net_mg_rate, 5)
+    top_40_nmr.sort_values(by=["REF_AREA", "OBS_VALUE"], inplace=True)
+    top_40_nmr_countries = top_40_nmr["REF_AREA"].unique()
+    top_40_others = this_df[this_df["REF_AREA"].isin(top_40_nmr_countries)]               
+    top_40_merged = pd.concat([top_40_nmr,top_40_others])
+    top_40_merged.sort_values(by=["REF_AREA", "TIME_PERIOD", "INDICATOR"], inplace=True)
+    ###############################################################
+    
+    # Enumerate columns
+    top_40_merged.replace({"REF_AREA": REF_AREA_dict}, inplace=True)
+    this_df.replace({"REF_AREA": REF_AREA_dict}, inplace=True)
 
 
     show_tables = "no"
-    show_tables = st.checkbox("Show Dataframe")
-
-    row1a_col1, row1a_col2 = st.columns([4, 4])
+    show_tables = st.checkbox("Show Dataframes")
 
     if show_tables:
-        # with row1a_col1:
-        st.write(indicator_df)
+        row1a_col1, row1a_col2 = st.columns([4, 4])
 
-        # with row1a_col2:
-        #     indicator_data_cols = get_data_columns(inventory_df, scale.lower(), frequency.lower())
-        #     indicator_df = indicator_df[indicator_df.INDICATOR==this_indicator]
-        #     st.write(f"#### {this_indicator} Layer")
-        #     st.write(indicator_df)
+        with row1a_col1:
+            st.write("**All Countries, All Indicators (2012 - 2022)**")
+            st.write(this_df)
 
-    chart_df = indicator_df.copy()
+        with row1a_col2:
+            # indicator_data_cols = get_data_columns(inventory_df, scale.lower(), frequency.lower())
+            # indicator_df = indicator_df[indicator_df.INDICATOR==this_indicator]
+            # st.write(f"#### {this_indicator} Layer")
+            st.write("**Top-10 NMR Countries, All Indicators (2012 - 2022)**")
+            st.write(top_40_merged)
 
-    # nice_plot = chart_df.hvplot.line(x="REF_AREA", y="OBS_VALUE", by="TIME_PERIOD", rot=55, title=str(selected_year))
-    nice_plot = chart_df.hvplot.line(x="REF_AREA", y="OBS_VALUE", by="TIME_PERIOD", rot=55, width=1000, height=500)
-    st.write(hv.render(nice_plot, backend='bokeh'))
 
-    # def show_chart(value):
-    #     mydata = chart_df[chart_df.TIME_PERIOD==value].hvplot.line(x="REF_AREA", y="OBS_VALUE", by="TIME_PERIOD", rot=55, title=value)
-    #     # pane = pn.panel(mydata)
-    #     return pane
+    top_40_nmr_sorted = top_40_merged.sort_values(by=["REF_AREA", "OBS_VALUE"]).copy()
+    nice_plot1 = top_40_nmr_sorted[top_40_nmr_sorted.INDICATOR=="DM_NET_MG_RATE"].hvplot.box( y="OBS_VALUE", by="REF_AREA", legend=False, rot=45, width=900, height=500)
+    nice_plot2 = top_40_merged[top_40_merged.INDICATOR==this_indicator].hvplot.line(
+        x="REF_AREA", y="OBS_VALUE", by="TIME_PERIOD", legend_position='top_left', rot=45, width=900, height=500)
+    st.write(hv.render(nice_plot1*nice_plot2, backend='bokeh'))
 
-    # ind = "Indicator Comparison preview"
-    # # chart_df = chart_df[chart_df.loc[:, 'OBS_VALUE'] > 30]
+    # row2_col1, row2_col2 = st.columns([4, 4])
+    # with row2_col1:
+    #     top_40_nmr_sorted = top_40_merged.sort_values(by=["REF_AREA", "OBS_VALUE"]).copy()
+    #     nice_plot1 = top_40_nmr_sorted[top_40_nmr_sorted.INDICATOR=="DM_NET_MG_RATE"].hvplot.box( y="OBS_VALUE", by="REF_AREA", rot=55, width=900, height=500)
+    #     # st.write(hv.render(nice_plot1, backend='bokeh'))
 
-    # ind_list = chart_df["INDICATOR"].unique()
+    # with row2_col2:
+    #     nice_plot2 = top_40_merged[top_40_merged.INDICATOR==this_indicator].hvplot.line(x="REF_AREA", y="OBS_VALUE", by="TIME_PERIOD", rot=55, width=900, height=500)
+    #     st.write(hv.render(nice_plot1*nice_plot2, backend='bokeh'))
 
-    # show_chart(selected_year)
-
-    # return None
 
 app()
