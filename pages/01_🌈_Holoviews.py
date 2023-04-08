d915241a1fc5b7b07f8571a20885fcebfd9705dd
Harry Oestreicher <harryo1968@gmail.com>
2023-04-08 09:29:28 -0700

    little update

--- pages/01_🌈_Holoviews.py
+++ pages/01_🌈_Holoviews.py
@@ -72,11 +72,19 @@ data_links = {
 #     df = df[df.TIME_PERIOD==2021]
 #     return df
 
+# @st.cache_data
+# def get_data(url):
+#     df = pd.read_csv(url)
+#     df.drop(columns=["Unnamed: 0"], inplace=True)
+#     # df = df[df.TIME_PERIOD==2021]
+#     return df
+
 @st.cache_data
-def get_data(url):
+def get_indicator_data(url):
     df = pd.read_csv(url)
     df.drop(columns=["Unnamed: 0"], inplace=True)
     # df = df[df.TIME_PERIOD==2021]
+    df.replace({"INDICATOR": INDICATOR_dict}, inplace=True)
     return df
 
 def get_indicator_dict(name):
@@ -111,31 +119,65 @@ def app():
     # manually setting these for now
     frequency = "annual"
     scale = "countries"
+    # Associate these group labels with thier file prefixes:
+    indicator_group_dict = {
+            "Demographic": "DM_",
+            "Economic": "ECON_",
+            "Migratory": "MG_",
+            "Maternal, Newborn, and Child Health": "MNCH_",
+            "Post-Trauma": "PT_",
+            "Water Services": "WS_"
+    }
 
-    df = get_data(data_links["indicator"]["_ALL_"])
-    years_list = [2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
-    this_df = df.copy()
+    # Lets fix how we're getting this data. We change to getttiign by group to make the UI more understandable.
+    df = get_indicator_data(data_links["indicator"]["_ALL_"])
+
+    # years_list = [2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
+    # indicator_df = df.copy()
 
     row1_col1, row1_col2  = st.columns([4, 4])
 
+
     with row1_col1:
-        # selected_col = "OBS_VALUE" #st.selectbox("Attribute", data_cols, 4)
-        # st.write(indicator_df.head(3))
-        this_ind_list = get_indicators(this_df)
-        # st.write(type(this_ind_list))
-        # this_ind_list = this_ind_list.sort()
-        this_indicator = st.selectbox("**Indicator:**", sorted(this_ind_list))
-        this_indicator_text = get_indicator_dict(this_indicator)
-        # Display the indicator full text
-        st.write(f"**{this_indicator_text}**")
-        # st.write(f"Data type: `{this_df.OBS_VALUE.dtype}`")
+        # indicator_group = st.selectbox("**Risk Factor Group**", ["DM_", "ECON_", "MG_", "MNCH_", "PT_", "WS_"])
+        indicator_group_selected = st.selectbox("**Indictator Group**", ["Demographic", "Economic", "Migratory", "Maternal, Newborn, and Child Health", "Post-Trauma", "Water Services"])
+        indicator_group = indicator_group_dict[indicator_group_selected]
+        
+    # Get Indicator Group data
+    indicator_df = get_indicator_data(data_links["indicator"][indicator_group])
+    indicator_df = indicator_df[indicator_df.INDICATOR!="Net migration rate (per 1,000 population)"]
+    # this_df = this_df[this_df.TIME_PERIOD==selected_year]
 
     with row1_col2:
-        st.write(".")
+        # selected_col = "OBS_VALUE" #st.selectbox("Attribute", data_cols, 4)
+        # st.write(indicator_df.head(3))
+
+        this_ind_list = indicator_df["INDICATOR"].unique().tolist()
+        this_indicator = st.selectbox(f"**{indicator_group_selected} Indicators**", this_ind_list)
+        # this_indicator_text = get_indicator_dict(this_indicator)
+
+        # Display the indicator full text
+        # st.write(this_indicator_text)
+
+
+    # with row1_col1:
+    #     # selected_col = "OBS_VALUE" #st.selectbox("Attribute", data_cols, 4)
+    #     # st.write(indicator_df.head(3))
+    #     this_ind_list = get_indicators(this_df)
+    #     # st.write(type(this_ind_list))
+    #     # this_ind_list = this_ind_list.sort()
+    #     this_indicator = st.selectbox("**Indicator:**", sorted(this_ind_list))
+    #     this_indicator_text = get_indicator_dict(this_indicator)
+    #     # Display the indicator full text
+    #     st.write(f"**{this_indicator_text}**")
+    #     # st.write(f"Data type: `{this_df.OBS_VALUE.dtype}`")
+
+    # with row1_col2:
+    #     st.write(".")
 
 
     ############################################################### Trim the fat 
-    net_mg_rate = this_df[this_df.INDICATOR=='DM_NET_MG_RATE'].sort_values('OBS_VALUE').copy()
+    net_mg_rate = df[df.INDICATOR=='Net migration rate (per 1,000 population)'].sort_values('OBS_VALUE').copy()
 
     def trim_the_fat(df, num):
         upp = df.sort_values('OBS_VALUE').head(num)
@@ -147,15 +189,14 @@ def app():
     top_40_nmr = trim_the_fat(net_mg_rate, 5)
     top_40_nmr.sort_values(by=["REF_AREA", "OBS_VALUE"], inplace=True)
     top_40_nmr_countries = top_40_nmr["REF_AREA"].unique()
-    top_40_others = this_df[this_df["REF_AREA"].isin(top_40_nmr_countries)]               
+    top_40_others = indicator_df[indicator_df["REF_AREA"].isin(top_40_nmr_countries)]               
     top_40_merged = pd.concat([top_40_nmr,top_40_others])
     top_40_merged.sort_values(by=["REF_AREA", "TIME_PERIOD", "INDICATOR"], inplace=True)
     ###############################################################
     
     # Enumerate columns
     top_40_merged.replace({"REF_AREA": REF_AREA_dict}, inplace=True)
-    this_df.replace({"REF_AREA": REF_AREA_dict}, inplace=True)
-
+    top_40_others.replace({"REF_AREA": REF_AREA_dict}, inplace=True)
 
     show_tables = "no"
     show_tables = st.checkbox("Show Dataframes")
@@ -164,22 +205,22 @@ def app():
         row1a_col1, row1a_col2 = st.columns([4, 4])
 
         with row1a_col1:
-            st.write("**All Countries, All Indicators (2012 - 2022)**")
-            st.write(this_df)
+            st.write("**All Countries (2012 - 2022)**")
+            st.write(top_40_nmr)
 
         with row1a_col2:
             # indicator_data_cols = get_data_columns(inventory_df, scale.lower(), frequency.lower())
             # indicator_df = indicator_df[indicator_df.INDICATOR==this_indicator]
             # st.write(f"#### {this_indicator} Layer")
-            st.write("**Top-10 NMR Countries, All Indicators (2012 - 2022)**")
-            st.write(top_40_merged)
+            st.write("**Top-10 NMR Countries (2012 - 2022)**")
+            st.write(top_40_others)
 
 
     top_40_nmr_sorted = top_40_merged.sort_values(by=["REF_AREA", "OBS_VALUE"]).copy()
     nice_plot1 = top_40_nmr_sorted[top_40_nmr_sorted.INDICATOR=="DM_NET_MG_RATE"].hvplot.box( y="OBS_VALUE", by="REF_AREA", legend=False, rot=45, width=500, height=500)
     nice_plot2 = top_40_merged[top_40_merged.INDICATOR==this_indicator].hvplot.line(
-        x="REF_AREA", y="OBS_VALUE", by="TIME_PERIOD", legend_position='top_left', rot=45, width=500, height=500)
-    st.write(hv.render(nice_plot1, backend='bokeh'))
+        x="REF_AREA", y="OBS_VALUE", by="TIME_PERIOD", legend_position='top_left', rot=45)
+    st.write(hv.render(nice_plot2, backend='bokeh'))
     # st.write(hv.render(nice_plot2, backend='bokeh'))
 
     # row2_col1, row2_col2 = st.columns([4, 4])
